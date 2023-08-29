d934e75cf76c1ffb48ff6d9d4c74af926368e340
Ali Nawab <alinawab9830@gmail.com>
2023-08-28 19:08:09 -0500

    new link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/YaBoiAli/React-Portfolio"
+                href="https://github.com/YaBoiAli/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
